39396568f2829d163c4f9c4833dfa7b11e659d91
ExtevaXT <96924741+ExtevaXT@users.noreply.github.com>
2024-04-06 12:10:10 +0500

    new ping

--- ping.py
+++ ping.py
@@ -3,15 +3,25 @@ import platform
 import ctypes
 from config import *
 import time
-
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
 if platform.system() == 'Windows': ctypes.windll.kernel32.SetConsoleTitleW("Ping Pong")
 print('Ping Pong')
 while True:
     try:
-        response = requests.get(f"{api}/ping?key={key}&v=2")
-        print(response.json())
+        response = requests.get("https://steamcommunity.com/pointssummary/ajaxgetasyncconfig", 
+                                headers={'Cookie': f'steamLoginSecure={steam_cookie};'})
         if response.json()["success"]:
-            time.sleep(180)
-        else: time.sleep(60)
-    except:
+            token = response.json()["data"]["webapi_token"]
+            logging.info(token)
+            response = requests.post(f"{api}/ping-new?key={key}", json={"access_token": token})
+            logging.info(response.json())
+            if response.json()["success"]:
+                time.sleep(24*60*60)
+            else: time.sleep(60)
+        else: 
+            logging.info(response.json())
+            time.sleep(60)
+    except Exception as e: 
+        logging.info(e)
         time.sleep(60)
